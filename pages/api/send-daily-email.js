d4d5f460536c19ccd863410c2743129e13023bd7
Paul Davidson <pauldavidson@Pauls-MacBook-Pro.local>
2025-04-12 11:10:02 -0700

    Update email

--- pages/api/send-daily-email.js
+++ pages/api/send-daily-email.js
@@ -409,7 +409,7 @@ data.forEach((lineup) => {
             <img src="https://best-concert-ever.vercel.app/bcefinalemaillogo.png" 
                 alt="Best Concert Ever logo" 
                 width="600" height="200" 
-                style="display: block; margin: 0 auto;" />
+        style="display: block; margin: 0 auto;" />
           </div>
 
           <div style="text-align: center; margin-bottom: 32px;">
@@ -432,16 +432,12 @@ data.forEach((lineup) => {
           </div>
 
           <div style="text-align: center; margin-bottom: 30px;">
-  <table align="center" style="margin: 0 auto; background-color: #111; border: 1px solid #444; border-radius: 12px;">
-    <tr>
-      <td style="padding: 10px;">
-        <img src="${headlinerImg}" alt="${headliner}" width="180" height="180" style="border-radius: 12px; display: block; margin: 0 auto;" />
-        <div style="font-weight: bold; text-transform: uppercase; margin-top: 10px; text-align: center;">${headliner}</div>
-        <div style="font-style: italic; font-size: 12px; color: #aaa; text-align: center;">Headliner</div>
-      </td>
-    </tr>
-  </table>
-</div>
+            <div style="border: 1px solid #444; border-radius: 12px; padding: 10px; background-color: #111; width: 200px; margin: 0 auto;">
+              <img src="${headlinerImg}" alt="${headliner}" style="width: 180px; height: 180px; border-radius: 12px;" />
+              <div style="font-weight: bold; text-transform: uppercase; margin-top: 10px;">${headliner}</div>
+              <div style="font-style: italic; font-size: 12px; color: #aaa;">Headliner</div>
+            </div>
+          </div>
 
           <div style="display: flex; justify-content: space-between; max-width: 420px; margin: 0 auto 40px;">
             <div style="text-align: center;">
