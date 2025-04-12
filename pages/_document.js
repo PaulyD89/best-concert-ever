acf616bd9886a68a50b04727dc2fab30542d3990
Paul Davidson <pauldavidson@Pauls-MacBook-Pro.local>
2025-04-12 15:03:14 -0700

    Add social sharing meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:title" content="Best. Concert. Ever." />
+        <meta property="og:description" content="Assemble the ultimate concert lineup!" />
+        <meta property="og:image" content="https://bestconcertevergame.com/social-preview.jpg" />
+        <meta property="og:url" content="https://bestconcertevergame.com" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Best. Concert. Ever." />
+        <meta name="twitter:description" content="Play the daily concert lineup game!" />
+        <meta name="twitter:image" content="https://bestconcertevergame.com/social-preview.jpg" />
+
         <script
           defer
           data-domain="bestconcertevergame.com"
