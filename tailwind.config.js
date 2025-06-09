face818d1feedddf5d6022ac3ae133af266d556e
Paul Davidson <pauldavidson@Pauls-MacBook-Pro.local>
2025-06-09 13:39:48 -0700

    add fade in for how to play infographic

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,18 @@ module.exports = {
       "animate-flicker"
     ],
     theme: {
-      extend: {}
+  extend: {
+    animation: {
+      fadeIn: 'fadeIn 0.6s ease-in forwards',
     },
+    keyframes: {
+      fadeIn: {
+        '0%': { opacity: '0' },
+        '100%': { opacity: '1' },
+      },
+    },
+  }
+},
     plugins: []
   }
   
