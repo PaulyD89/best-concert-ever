4ba79521400b52e30d1548691de3b902051e1eb7
Paul Davidson <pauldavidson@Pauls-MacBook-Pro.local>
2025-07-25 15:07:30 -0700

    metapixel

--- pages/_document.js
+++ pages/_document.js
@@ -20,10 +20,38 @@ export default function Document() {
           data-domain="bestconcertevergame.com"
           src="https://plausible.io/js/script.js"
         />
+
+        {/* Meta Pixel Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1067195892216515');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
+
+        {/* Meta Pixel noscript fallback */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <img height="1" width="1" style="display:none"
+              src="https://www.facebook.com/tr?id=1067195892216515&ev=PageView&noscript=1" />
+            `,
+          }}
+        />
       </body>
     </Html>
   );
