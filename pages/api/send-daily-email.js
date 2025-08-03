3981856e980c70a0eab30b40dc7230e42aa1ab5c
Paul Davidson <pauldavidson@Pauls-MacBook-Pro.local>
2025-08-03 13:43:22 -0700

    fix spacing issue

--- pages/api/send-daily-email.js
+++ pages/api/send-daily-email.js
@@ -157,20 +157,15 @@ export default async function handler(req, res) {
           <a href="https://bestconcertevergame.com" style="color: black; font-weight: bold; text-decoration: underline;">Submit your own lineup</a>
         </div>
 
-        <tr>
+      <tr>
   <td align="center" style="padding: 20px 0;">
     <p style="font-family: Arial, sans-serif; font-size: 16px; color: #000; font-weight: bold; margin: 0 0 10px;">
       Follow us on social:
     </p>
-    <a href="https://instagram.com/bestconcertevergame" target="_blank" style="margin: 0 10px;">
-      <img src="https://bestconcertevergame.com/icons/instagram.png" width="32" height="32" alt="Instagram" style="display:inline-block; border:0;">
-    </a>
-    <a href="https://x.com/bestconcertgame" target="_blank" style="margin: 0 10px;">
-      <img src="https://bestconcertevergame.com/icons/twitter.png" width="32" height="32" alt="Twitter" style="display:inline-block; border:0;">
-    </a>
+    <a href="https://instagram.com/bestconcertevergame" target="_blank" style="margin: 0 10px; text-decoration: none; display: inline-block;"><img src="https://bestconcertevergame.com/icons/instagram.png" width="32" height="32" alt="Instagram" style="display:inline-block; border:0;"></a>
+    <a href="https://x.com/bestconcertgame" target="_blank" style="margin: 0 10px; text-decoration: none; display: inline-block;"><img src="https://bestconcertevergame.com/icons/twitter.png" width="32" height="32" alt="Twitter" style="display:inline-block; border:0;"></a>
   </td>
 </tr>
-
 
         <tr>
   <td align="center">
