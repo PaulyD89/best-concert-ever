0ce7a8b7a20f7201382d32c6f614e9186ffb0695
Paul Davidson <pauldavidson@Pauls-MacBook-Pro.local>
2025-09-17 14:42:42 -0700

    new top badge

--- pages/index.js
+++ pages/index.js
@@ -976,7 +976,7 @@ console.log("Lineup submitted:", { headliner, opener, secondOpener });
   className="relative bg-[#fdf6e3] text-black rounded-xl p-6 w-full max-w-md text-center border-2 border-yellow-400 shadow-[0_0_15px_4px_rgba(253,224,71,0.8)] bg-[url('/bcefans.jpg')] bg-cover bg-center"
 >
         <div className="absolute -top-12 left-1/2 transform -translate-x-1/2 w-32 h-32 bg-white rounded-full border-4 border-black shadow-lg flex items-center justify-center overflow-hidden">
-          <img src="/logo.png" alt="Best Concert Ever Logo" className="w-full h-full object-cover" style={{ objectFit: "cover", objectPosition: "center" }} crossOrigin="anonymous" />
+          <img src="/new-top-badge.png" alt="Best Concert Ever Logo" className="w-full h-full object-cover" style={{ objectFit: "cover", objectPosition: "center" }} crossOrigin="anonymous" />
         </div>
 
         <div className="mt-16 mb-4 text-base font-extrabold uppercase tracking-widest text-black inline-block px-4 py-1 border-2 border-black rotate-[-2deg] bg-white shadow-md font-mono">
