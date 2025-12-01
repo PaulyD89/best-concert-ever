0b3b3fb1dcdca4f2e73e2190d24b8c587749294b
Paul Davidson <paulsd@mac.com>
2025-12-01 12:42:15 -0800

    update ad

--- pages/index.js
+++ pages/index.js
@@ -2744,7 +2744,7 @@ if (!error) {
               {userMarket === 'US' && (
                 <div className="mt-4">
                   <img 
-                    src="/promotercontest2.png" 
+                    src="/promotercontestdec1.png" 
                     alt="December Promoter Giveaway - Win $50"
                     className="w-full rounded-lg hover:scale-[1.01] transition-transform cursor-pointer"
                     onClick={() => setShowContestRules(true)}
